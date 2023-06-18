57535c9383dd09dcd5b3c52641e278754a6915ef
Darko <132366394+darkomoranini@users.noreply.github.com>
2023-06-18 12:51:44 +0200

    Add files via upload

---
+++ Optica_corregido.sql
@@ -0,0 +1,240 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 18-06-2023 a las 12:49:24
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `Optica`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `dirección postal` varchar(200) NOT NULL,
+  `telefono` varchar(45) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `fecha de registro` datetime NOT NULL,
+  `recomendación_cliente` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`id`, `nombre`, `dirección postal`, `telefono`, `email`, `fecha de registro`, `recomendación_cliente`) VALUES
+(1, 'Darko Morandini', 'Calle X numero 2, 08003. Barcelona', '666666666', 'darkom@gmail.com', '2023-06-09 16:44:24', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleado`
+--
+
+CREATE TABLE `empleado` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `empleado`
+--
+
+INSERT INTO `empleado` (`id`, `nombre`) VALUES
+(1, '');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `gafas`
+--
+
+CREATE TABLE `gafas` (
+  `id` int(11) NOT NULL,
+  `marca` varchar(45) NOT NULL,
+  `graduación izq` float NOT NULL,
+  `graduación der` float NOT NULL,
+  `montura` enum('flotante','pasta','metalica','') NOT NULL,
+  `color cristal izq` varchar(45) NOT NULL,
+  `color cristal der` varchar(45) NOT NULL,
+  `color montura` varchar(45) NOT NULL,
+  `precio` float NOT NULL,
+  `empleado_id` int(11) NOT NULL,
+  `cliente_fk` int(11) DEFAULT NULL,
+  `marcas__id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `gafas`
+--
+
+INSERT INTO `gafas` (`id`, `marca`, `graduación izq`, `graduación der`, `montura`, `color cristal izq`, `color cristal der`, `color montura`, `precio`, `empleado_id`, `cliente_fk`, `marcas__id`) VALUES
+(1, 'primera', 0.2, 0.9, 'pasta', 'azul', 'azul', 'azul', 99.99, 1, NULL, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Marcas`
+--
+
+CREATE TABLE `Marcas` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `proveedor_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `Marcas`
+--
+
+INSERT INTO `Marcas` (`id`, `nombre`, `proveedor_id`) VALUES
+(1, 'D&G', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedor`
+--
+
+CREATE TABLE `proveedor` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `dirección` varchar(200) NOT NULL,
+  `telefono` varchar(45) NOT NULL,
+  `fax` varchar(45) NOT NULL,
+  `nif` varchar(10) NOT NULL,
+  `gafas_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `proveedor`
+--
+
+INSERT INTO `proveedor` (`id`, `nombre`, `dirección`, `telefono`, `fax`, `nif`, `gafas_id`) VALUES
+(1, 'proveedor gafas', 'Calle Y, 1. 08001. Barcelona', '5555555555', 'ola@fax.com', '29539717F', 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `recomendación_id` (`recomendación_cliente`);
+
+--
+-- Indices de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `gafas`
+--
+ALTER TABLE `gafas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `empleado_fk` (`empleado_id`),
+  ADD KEY `cliente_id` (`cliente_fk`);
+
+--
+-- Indices de la tabla `Marcas`
+--
+ALTER TABLE `Marcas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `proveedor_fk` (`proveedor_id`);
+
+--
+-- Indices de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `gafas_fk` (`gafas_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `gafas`
+--
+ALTER TABLE `gafas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `Marcas`
+--
+ALTER TABLE `Marcas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD CONSTRAINT `recomendación_id` FOREIGN KEY (`recomendación_cliente`) REFERENCES `cliente` (`id`);
+
+--
+-- Filtros para la tabla `gafas`
+--
+ALTER TABLE `gafas`
+  ADD CONSTRAINT `cliente_id` FOREIGN KEY (`cliente_fk`) REFERENCES `cliente` (`id`),
+  ADD CONSTRAINT `empleado_fk` FOREIGN KEY (`empleado_id`) REFERENCES `empleado` (`id`);
+
+--
+-- Filtros para la tabla `Marcas`
+--
+ALTER TABLE `Marcas`
+  ADD CONSTRAINT `proveedor_fk` FOREIGN KEY (`proveedor_id`) REFERENCES `proveedor` (`id`);
+
+--
+-- Filtros para la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  ADD CONSTRAINT `gafas_fk` FOREIGN KEY (`gafas_id`) REFERENCES `gafas` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
